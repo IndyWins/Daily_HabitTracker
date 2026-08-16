74d999e7a9627e1c42e3bb25ad32aa7897a175dd
IndyWins <179518152+IndyWins@users.noreply.github.com>
2026-08-16 11:23:39 +0100

    Initial Commit

---
+++ Daily_HabitTrackerUI/Daily_HabitTrackerUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_HabitTrackerUIApp.swift
+//  Daily_HabitTrackerUI
+//
+//  Created by Richard Clarke on 16/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct Daily_HabitTrackerUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
